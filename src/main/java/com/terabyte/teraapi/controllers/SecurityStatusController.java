c7ba7d010086cbd6dedf16717f0a9bf0745f9234
david-sfernandes <62266135+david-sfernandes@users.noreply.github.com>
2024-01-09 22:04:26 -0300

    Get Bitdefender data

--- src/main/java/com/terabyte/teraapi/controllers/SecurityStatusController.java
+++ src/main/java/com/terabyte/teraapi/controllers/SecurityStatusController.java
@@ -5,19 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.terabyte.teraapi.models.SecurityStatus;
 import com.terabyte.teraapi.repositories.SecurityStatusRepository;
+import com.terabyte.teraapi.services.BitdefenderService;
 
 @RestController
 @RequestMapping("/api/security-status")
 public class SecurityStatusController {
   @Autowired
   private final SecurityStatusRepository securityStatusRepository = new SecurityStatusRepository();
+  @Autowired
+  private final BitdefenderService bitdefenderService = new BitdefenderService();
 
   @GetMapping()
   public List<SecurityStatus> getSecurityStatuses() {
     return securityStatusRepository.getAll();
+  }
+
+  @GetMapping("/sync")
+  public String syncSecurityStatuses() {
+    return bitdefenderService.loadGroups();
+  }
+
+  @GetMapping("/syncId")
+  public String syncSecurityStatuses(@RequestParam String id) {
+    return bitdefenderService.loadStatusByGroupId(id);
   }
 }
